c872a811dedfdb292c8118ba5f9b184d61a88abf
nata <vwenera@yandex.ru>
2022-11-28 19:07:27 +0700

    reshenie

--- Homework/Zadanie_4/Reshenie/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
-    while(true)
-    {
-        string input = Console.ReadLine();
-        if(!input.Equals("exit"))
-            Console.WriteLine("{0}->{1}",input, input[1]);
-        else
-            break;
-    }
